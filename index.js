22660530cf1be7ad0960c7dc5a5cdd6d803d0dce
Abhi <abhi@finpace.com>
2024-11-11 19:32:55 +0530

    minor fixes

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
 const { convertToHtml } = require("./src/handlers/pdf/convert-to-html");
+const fs = require("fs");
+const { S3Client } = require("@aws-sdk/client-s3");
 
-exports.handler = async () => {
-  const html = await convertToHtml("./orig.pdf");
-  const response = {
-    statusCode: 200,
-    body: html,
+const client = new S3Client({ region: "us-east-1" });
+
+console.log("Hello from Lambda!");
+
+const getFileFromS3 = async (Bucket, Key) => {
+  const params = {
+    Bucket,
+    Key,
   };
-  return response;
+  const res = await client.send(new GetObjectCommand(params));
+  return res.Body;
+};
+
+exports.handler = async (event) => {
+  const Key = event.Records[0].s3.object.key;
+  const Bucket = event.Records[0].s3.bucket.name;
+  console.log("Key: ", Key);
+  console.log("Bucket: ", Bucket);
+
+  const file = await getFileFromS3(Bucket, Key);
+
+  console.log("read file from s3");
+
+  // write to lambda temp dir
+  const filePath = `/tmp/${Key}`;
+  fs.writeFileSync(filePath, file);
+
+  const { outputFile } = await convertToHtml(filePath);
+
+  console.log("converted to html");
+
+  await client.send(
+    new PutObjectCommand({
+      Bucket,
+      Key: Key.replace(".pdf", ".html"),
+      Body: outputFile,
+    })
+  );
+
+  console.log("uploaded to s3");
 };
