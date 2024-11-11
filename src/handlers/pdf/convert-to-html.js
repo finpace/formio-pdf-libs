8cff440fe1d0806e5ebf87909ac10f9868ac043a
Abhi <abhi@finpace.com>
2024-11-11 23:19:40 +0530

    working code

--- src/handlers/pdf/convert-to-html.js
+++ src/handlers/pdf/convert-to-html.js
@@ -1,12 +1,16 @@
 import path from "path";
 import fs from "fs";
 import { v4 as uuid } from "uuid";
+import { tmpdir } from "os";
+
+console.log("tmpdir", tmpdir());
 
 import { htmlGenerationTimeoutConfig } from "../../../config.js";
 import { generateHtml } from "../../services/pdf/convert-to-html/generate-html.js";
 
 export const convertToHtml = async (filePath, optimizedPdf = false) => {
   const outputFileName = `${uuid()}.html`;
+  const outputPath = path.join(tmpdir(), `${outputFileName}`);
   const { timeout, backoff } = htmlGenerationTimeoutConfig;
   const calculatedTimeout = optimizedPdf ? timeout + backoff : timeout;
   try {
@@ -17,7 +21,7 @@ export const convertToHtml = async (filePath, optimizedPdf = false) => {
         zoom: 1.78,
         dpi: 144,
       },
-      ["--dest-dir", "/tmp"],
+      ["--dest-dir", tmpdir()],
       calculatedTimeout
     );
   } catch (err) {
@@ -25,6 +29,6 @@ export const convertToHtml = async (filePath, optimizedPdf = false) => {
   }
   return {
     outputFileName,
-    outputFile: fs.readFileSync(`/tmp/${outputFileName}`, "utf8"),
+    outputFile: fs.readFileSync(outputPath, "utf8"),
   };
 };
--- src/services/pdf/convert-to-html/generate-html.js
+++ src/services/pdf/convert-to-html/generate-html.js
@@ -1,5 +1,10 @@
 import { pdf2htmlexPath } from "../../../../config.js";
 import { exec } from "../../../utils/exec.js";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export const generateHtml = async (filePath, toFile, params, commands, timeout) => {
   const { zoom, dpi } = params;
